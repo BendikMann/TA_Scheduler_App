86930c20a01599252f397c275c0ba39f19fe41c6
matth <lawre247@uwm.edu>
2022-11-29 19:17:25 -0600

    test classes for admin, instructor, and ta

--- tests/unit_tests/test_user.py
+++ tests/unit_tests/test_user.py
@@ -1,5 +1,60 @@
 from unittest import TestCase
+from TA_Scheduler.models import Account
+from TA_Scheduler.user import Admin, Instructor, Ta
+
+class TestAdmin(TestCase):
+
+    def TestAdminInit(self):
+        test_acct = Account
+        pass
+
+    def TestAdminInitWrongType(self):
+        test_string = "hello"
+        test_int = 1
+
+        with self.assertRaises(ValueError, msg="ValueError not raised when passing Admin a string"):
+            a = Admin(test_string)
+
+        with self.assertRaises(ValueError, msg="ValueError not raised when passing Admin an integer"):
+            a = Admin(test_int)
+
+    def TestAdminInitWrongAccountType(self):
+        pass
 
 
 class TestInstructor(TestCase):
-    pass
+
+    def TestInstructorInit(self):
+        pass
+
+    def TestInstructorInitWrongType(self):
+        test_string = "hello"
+        test_int = 1
+
+        with self.assertRaises(ValueError, msg="ValueError not raised when passing Instructor a string"):
+            a = Instructor(test_string)
+
+        with self.assertRaises(ValueError, msg="ValueError not raised when passing Instructor an integer"):
+            a = Instructor(test_int)
+
+    def TestInstructorInitWrongAccountType(self):
+        pass
+
+
+class TestTa(TestCase):
+
+    def TestTaInit(self):
+        pass
+
+    def TestTaInitWrongType(self):
+        test_string = "hello"
+        test_int = 1
+
+        with self.assertRaises(ValueError, msg="ValueError not raised when passing Ta a string"):
+            a = Ta(test_string)
+
+        with self.assertRaises(ValueError, msg="ValueError not raised when passing Ta an integer"):
+            a = Ta(test_int)
+
+    def TestTaInitWrongAccountType(self):
+        pass
