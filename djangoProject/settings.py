d1b723444c2e8798ba8b3b860720f0b0d5bd5480
ddoyle20 <ddoyle@uwm.edu>
2022-12-06 11:12:25 -0600

    added code to settings.py to use outlook as the SMTP server

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -125,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'tascheduler@outlook.com'
+EMAIL_HOST_PASSWORD = 'cnqengnmdbqaadif'
