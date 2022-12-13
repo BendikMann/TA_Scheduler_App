aa3a4e264d39812979ae71c1961df76f977f1fd9
Bendik Mann <that.that.this@outlook.com>
2022-12-13 16:47:04 -0600

    Fix final issues with Factories importing incorrectly.

--- tests/unit_tests/test_user.py
+++ tests/unit_tests/test_user.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from tests.Factories import *
-from tests import Factories
 from TA_Scheduler import user
 import random
 from faker import Faker
@@ -30,32 +29,32 @@ class UserInitTests(TestCase):
         self.random_string = self.fake.word()
         self.random_integer = random.randint(0, 999999)
 
-        self.blank_account = Factories.UserFactory.create()
+        self.blank_account = UserFactory.create()
         self.blank_account.groups.clear()
 
-        self.admin_account = Factories.UserFactory.create()
+        self.admin_account = UserFactory.create()
         self.admin_account.groups.clear()
         self.admin_account.groups.add(Group.objects.get(name="Admin"))
 
-        self.instructor_account = Factories.UserFactory.create()
+        self.instructor_account = UserFactory.create()
         self.instructor_account.groups.clear()
         self.instructor_account.groups.add(Group.objects.get(name="Instructor"))
 
-        self.ta_account = Factories.UserFactory.create()
+        self.ta_account = UserFactory.create()
         self.ta_account.groups.clear()
         self.ta_account.groups.add(Group.objects.get(name="TA"))
 
-        self.admin_instructor_account: User = Factories.UserFactory.create()
+        self.admin_instructor_account: User = UserFactory.create()
         self.admin_instructor_account.groups.clear()
         self.admin_instructor_account.groups.add(Group.objects.get(name="Admin"))
         self.admin_instructor_account.groups.add(Group.objects.get(name="Instructor"))
 
-        self.ta_instructor_account: User = Factories.UserFactory.create()
+        self.ta_instructor_account: User = UserFactory.create()
         self.ta_instructor_account.groups.clear()
         self.ta_instructor_account.groups.add(Group.objects.get(name="TA"))
         self.ta_instructor_account.groups.add(Group.objects.get(name="Instructor"))
 
-        self.admin_ta_account: User = Factories.UserFactory.create()
+        self.admin_ta_account: User = UserFactory.create()
         self.admin_ta_account.groups.clear()
         self.admin_ta_account.groups.add(Group.objects.get(name="Admin"))
         self.admin_ta_account.groups.add(Group.objects.get(name="TA"))
